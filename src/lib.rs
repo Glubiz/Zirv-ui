1bd63b8aeafed84866e5717438157e04acb67bfe
Glubiz <josj@solskov-jensen.dk>
2024-04-17 15:51:37 +0200

    Removing image

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,18 @@
 mod flex;
 
 #[cfg(feature = "flex")]
-pub use flex::{
-    Flex,
-    FlexDirection,
-    FlexJustify,
-    FlexAlign,
-    FlexProps
-};
-
-#[cfg(feature = "image")]
-mod image;
-
-#[cfg(feature = "image")]
-pub use image::{
-    Image,
-    ImageProps
-};
+pub use flex::{Flex, FlexAlign, FlexDirection, FlexJustify, FlexProps};
 
 #[cfg(feature = "toast")]
 mod toast;
 
 #[cfg(feature = "toast")]
 pub use toast::{
-    Toast,
-    use_toast::use_toast,
-    manager::ToastManager,
-    utils::ToastType,
     component::{ToastComponent, ToastComponentProps},
     component_factory::ToastFactory,
-    provider::{ToastProvider, ToastProviderProps}
+    manager::ToastManager,
+    provider::{ToastProvider, ToastProviderProps},
+    use_toast::use_toast,
+    utils::ToastType,
+    Toast,
 };
